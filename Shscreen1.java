ac6978ec0d4d2900d30303bd240678f2b2f7010e
sarika2023 <sarikakuk123@gmail.com>
2023-08-22 10:59:16 +0530

    addition

--- Shscreen1.java
+++ Shscreen1.java
@@ -1,10 +1,20 @@
 package screen1;
 
+
+
 public class Shscreen1 {
+	
+	
 
 	public static void main(String[] args) {
+		Shscreen1 s=new Shscreen1();
+		s.m1();
 		System.out.println("Test case1");
 
+	}
+	void m1()
+	{
+		System.out.println("addition");
 	}
 
 }
